a1a8cd0bf7027e6ef0e9b3dbdc3d0f883eac8664
Jan Kocka <kockahonza@gmail.com>
2025-02-26 11:01:50 +0000

    working on NED

--- scripts/ned_firstlook1.jl
+++ scripts/ned_firstlook1.jl
@@ -1,7 +1,48 @@
 using DrWatson
 @quickactivate "TopoStochSim"
 
+using DataFrames
 using GLMakie
 
 using GraphModels
 using NonEqDigits
+
+function gsummary1(g)
+    # num of completely isolated nodes
+    isolated = []
+    # num of nodes with only incoming edges
+    deadends = []
+    # num of nodes with only outgoing edges
+    neverback = []
+    for ni in 1:nv(g)
+        if isempty(neighbors(g, ni))
+            push!(isolated, ni)
+        else
+            if isempty(outneighbors(g, ni))
+                push!(deadends, ni)
+            end
+            if isempty(inneighbors(g, ni))
+                push!(neverback, ni)
+            end
+        end
+    end
+    isolated, deadends, neverback
+end
+gsummary1(gm::AbstractGraphModel) = gsummary1(graph(gm))
+
+function makeruledf(N=3)
+    if !isa(N, AbstractVector)
+        N = [N]
+    end
+    df = DataFrame(numscc=Int[], numac=Int[], numss=Int[])
+    for code in 1:255
+        for n in N
+            ned = make_ce_ned(n, code)
+            numscc = length(strongly_connected_components(graph(ned)))
+            numac = length(attracting_components(graph(ned)))
+            numss = length(steadystates2(ned))
+            push!(df, (numscc, numac, numss))
+        end
+    end
+    df
+end
